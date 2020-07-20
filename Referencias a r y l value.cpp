9759884b15c5e3a7b14a81a086f648bf553fd570
AlvaroTimo <aticonamo@unsa.edu.pe>
2020-07-20 00:18:04 -0500

    Laboratorio7

--- Referencias a r y l value.cpp
+++ Referencias a r y l value.cpp
@@ -16,6 +16,14 @@ void printReference (string&& str)
 int main(){
     string me( "alex" );
     printReference(  me ); // calls the first printReference function, taking an lvalue reference
-    printReference( getName() ); // calls the 
+    printReference( getName() ); // calls the
+
+    string   s1     = "Hello ";
+    string   s2     = "world";
+    //const string &s_rref = s1 +s2(1);
+    //string& s_rref = s1 + s2(2);
+    string&& s_rref = s1 + s2;
+    s_rref += ", my friend";
+    cout << s_rref << '\n';  
     return 0;
 }
